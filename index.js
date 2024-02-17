ff90779ada3c194d7fbe6c5361f4d1956ca4f7fa
adnan <adnan232462@gmail.com>
2024-02-17 22:38:34 +0530

    final

--- index.js
+++ index.js
@@ -1,43 +1,58 @@
 const main = document.querySelector(".main");
 const form = document.getElementById("form");
+const text = document.querySelector(".text");
 const timerContainer = {};
 let timerId = 1;
+let itr = 0;
 form.addEventListener("submit" , (e) => {
     e.preventDefault();
     let hour = form.hh.value;
     let min = form.mm.value;
     let sec = form.ss.value;
-    console.log(hour +" " + min + " "+ sec);
-    const container = document.createElement("div");
-    container.className = "container";
-    container.id = timerId++;
-    container.innerHTML = `<div class="set-timer">
-        <div>TimeLeft :</div>
-        <div class="time">
-            <input type="text" name="hh" class="hh" value=${hour} readonly><span>:</span>
-            <input type="text" name="mm" class="mm" value=${min} readonly><span>:</span> 
-            <input type="text" name="ss" class="ss" value=${sec} readonly>
-        </div>
-        <button class="btn" onClick="deleteTimer(event)" >delete</button>
-    </div>`;
-    main.appendChild(container);
-    form.reset();
-    setTimer(hour , min , sec , container.id);
+    if(Number(min) < 60 && Number(sec) < 60){
+        // console.log(hour +" " + min + " "+ sec);
+        const container = document.createElement("div");
+        container.className = "container";
+        container.id = timerId++;
+        container.innerHTML = `<div class="set-timer">
+            <div>TimeLeft :</div>
+            <div class="time">
+                <input type="text" name="hh" class="hh" value=${hour} readonly><span>:</span>
+                <input type="text" name="mm" class="mm" value=${min} readonly><span>:</span> 
+                <input type="text" name="ss" class="ss" value=${sec} readonly>
+            </div>
+            <button class="btn" onClick="deleteTimer(event)">delete</button>
+        </div>`;
+        text.className = "text hide";
+        itr++;
+        main.appendChild(container);
+        form.reset();
+        setTimer(hour , min , sec , container.id);
+    }else{
+        alert("Invalid Time Format!");
+        form.reset();
+    }
 })
 
 function setTimer(hour , min , sec , id){
     let hours = parseInt(hour);
     let minutes = parseInt(min);
     let seconds = parseInt(sec);
-    const node = document.getElementById(id).children[0].children[1];
-    // console.log(node);
+    const nodeCont = document.getElementById(id);
+    const node = nodeCont.children[0].children[1];
     const interval = setInterval( () => {
         if (hours === 0 && minutes === 0 && seconds === 0) {
-            // alertSound.play();
+            nodeCont.style.backgroundColor = '#F0F758';
+            nodeCont.style.color = '#34344A';
+            nodeCont.innerText ='Timer Is Up!';
             clearInterval(interval);
             setTimeout(() => {
-            //   hourDisplay.parentNode.parentNode.remove();
-            }, 4000);
+                nodeCont.remove();
+               itr--;
+               if(itr===0){
+                text.classList.remove("hide");
+                }
+            }, 3000);
             return;
         }
         if(seconds === 0) {
@@ -65,4 +80,8 @@ function addTrailingZero(num){
 
 function deleteTimer(e){
     e.target.parentNode.parentNode.remove();
+    itr--;
+    if(itr===0){
+        text.classList.remove("hide");
+    }
 }
